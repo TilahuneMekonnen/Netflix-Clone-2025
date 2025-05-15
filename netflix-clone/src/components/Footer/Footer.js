b30d2d5474eb3898f5d0a7248143a2268413b3df
TilahuneMekonnen <tilahunemihreta@gmail.com>
2025-05-15 00:23:48 -0400

    footer Edit

--- netflix-clone/src/components/Footer/Footer.js
+++ netflix-clone/src/components/Footer/Footer.js
@@ -9,9 +9,15 @@ const Footer = () => {
     <div className="footer_outer_container">
       <div className="footer_socials">
         <ul>
-          <li><FacebookOutlinedIcon /></li>
-          <li><InstagramIcon /></li>
-          <li><YouTubeIcon /></li>
+          <li>
+            <FacebookOutlinedIcon />
+          </li>
+          <li>
+            <InstagramIcon />
+          </li>
+          <li>
+            <YouTubeIcon />
+          </li>
         </ul>
       </div>
 
@@ -19,18 +25,24 @@ const Footer = () => {
         <div className="footer_category social_category">
           <p>Audio Description</p>
           <p>Investor Relations</p>
+          <p>Legal Notice</p>
+          <span className="service-code">Service Code</span>
+          <p>© 1997-2025 Netflix, Inc.</p>
         </div>
         <div className="footer_category">
           <p>Help Center</p>
           <p>Jobs</p>
+          <p>Cookies Prefernce</p>
         </div>
         <div className="footer_category">
           <p>Gift Cards</p>
           <p>Terms of Use</p>
+          <p>Corporate Information</p>
         </div>
         <div className="footer_category">
           <p>Media Center</p>
           <p>Privacy</p>
+          <p>Contact us</p>
         </div>
       </div>
     </div>
